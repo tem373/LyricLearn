3d91ab59efec15b9cb1d08f0ae1bad09846da706
tem373 <tem373@nyu.edu>
2019-05-03 15:35:52 -0400

    Some work on the linreg part

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import torch
 import torch.utils.data
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import Ridge
 
 DATA_FILENAME = 'billboard_lyrics_1964-2015.csv'
 
@@ -26,8 +27,13 @@ os.path.exists(root_dir), os.path.exists(data_dir)
 full_dataset = pd.read_csv(os.path.join(data_dir, DATA_FILENAME), encoding='latin-1')
 train_len = int(0.8 * len(full_dataset))
 test_len = len(full_dataset) - train_len
-train, test = torch.utils.data.random_split(full_dataset, [train_len, test_len])
+train_rnn, test_rnn = torch.utils.data.random_split(full_dataset, [train_len, test_len])
+#train_sk, test_sk = 
 
-# "Naive" analysis with tfidf
+# "Naive" analysis with tfidf word frequency
+# Do linear regression using the Tf-idf as a feature
 vectorizer = TfidfVectorizer()
+corpus = list(full_dataset['Lyrics'].values.astype('U')) # Unicode column values from dataset
+X = vectorizer.fit_transform(corpus)
 
+# Use ridge regression (and regular linear regression?) - ridge regression helps eliminate collinearity
