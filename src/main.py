259ccf76b15e27aadac8ad05ef27484c5c472a5c
tem373 <tem373@nyu.edu>
2019-05-04 14:30:51 -0400

    TFIDF working now

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import torch.utils.data
 from scipy import sparse
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-#from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Ridge
 
 import lstm
@@ -22,9 +22,10 @@ os.path.exists(root_dir), os.path.exists(data_dir)
 
 # Load dataset
 full_dataset = pd.read_csv(os.path.join(data_dir, DATA_FILENAME), encoding='latin-1')
-train_len = int(0.8 * len(full_dataset))
-test_len = len(full_dataset) - train_len
-train, test = torch.utils.data.random_split(full_dataset, [train_len, test_len])
+x_train, x_test, y_train, y_test = train_test_split(list(full_dataset['Lyrics'].values.astype('U')), list(full_dataset['Year']))
+#train_len = int(0.8 * len(full_dataset))
+#test_len = len(full_dataset) - train_len
+#train, test = torch.utils.data.random_split(full_dataset, [train_len, test_len])
 
 ################################ "Naive" tf-idf analysis ####################################
 
@@ -33,15 +34,22 @@ vectorizer = TfidfVectorizer(lowercase=True, ngram_range=(1,3), max_features=200
 
 # Create vectors from train and test data - fit_transform() for training and transform() for testing
 # fit_transform() is basically fit() followed by transform()
-train_vec = vectorizer.fit_transform(list(train['Lyrics'].values.astype('U'))) # Unicode column values from dataset
-test_vec = vectorizer.transform(list(test['Lyrics'].values.astype('U')))
+train_vec = vectorizer.fit_transform(x_train) # Unicode column values from dataset
+test_vec = vectorizer.transform(x_test)
 
 # Build Ridge Model (linear regression plus regularization)
 clf = Ridge(alpha=1.0, random_state=1)
-y_train = train['Year']
+#y_train = train['Year']
 clf.fit(train_vec, y_train)
 
 # Validate on test data
 predictions = clf.predict(test_vec)
+rounded_predictions = [int(round(x)) for x in predictions]
+
+for i in range(0, len(rounded_predictions)):
+    print("Actual value: " + str(y_test[i]))
+    print("Predicted value: " + str(rounded_predictions[i]))
+    print("Error: " + str(abs(y_test[i] - rounded_predictions[i])))
+    print("")
 
 # Evaluate using sklearn metrics
