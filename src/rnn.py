f89d5b14b5a7bd899a93e0ea81018a05f76f7abf
Odikekachukwu Eze-Echesi <odikekachukwu@10-17-198-62.dynapool.wireless.nyu.edu>
2019-05-04 15:31:37 -0400

    rnn update

--- src/rnn.py
+++ src/rnn.py
@@ -6,6 +6,7 @@ from io import open
 data_filename = 'billboard_lyrics_1964-2015.csv'
 file = '../data/billboard_lyrics_1964-2015.csv'
 
+
 class RNN(torch.nn.Module):
     """ Module is the base class for all Pytorch NN modules"""
     def __init__(self):
@@ -17,7 +18,7 @@ class LSTM(torch.nn.Module):
     def __init__(self):
         pass
 
-
+# Creates a dict data structure mapping each song to the year it was produced
 def groupSongs(filename):
     lines = open(filename, encoding='latin-1').read().strip().split('\n')
     song_dict = {}
@@ -27,10 +28,18 @@ def groupSongs(filename):
             song_dict[line_list[4]] = line_list[3]
     return song_dict
 
+# How to convert the lyrics to tensors
+def lyricsToTensor(lyrics):
+    lyric_list = lyrics.split()
+    lyric_tensor = torch.cuda.FloatTensor(lyric_list)
+    return lyric_tensor
 
 s_dict = groupSongs(file)
 f_key = list(s_dict.keys())[1]
-print(f_key + " Year: " + s_dict[f_key])
+#print(f_key + " Year: " + s_dict[f_key])
+
+sample = "this is a sample lyric"
+print(lyricsToTensor(sample))
 
 
 
